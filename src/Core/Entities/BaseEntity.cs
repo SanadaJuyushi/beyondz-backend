886d09fc9ef321c083d64a70cf3b9740bc8421fa
dongsu <dongsu.workout@outlook.jp>
2020-09-30 14:58:13 +0900

    Remove default file

---
+++ src/Core/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Core.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
